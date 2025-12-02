f900d3138de2d47aa482a723abe338e5e6e889b0
sangnhc <sangbeau@gmail.com>
2025-12-03 04:31:05 +0700

    ok

--- app11.js
+++ app11.js
@@ -399,10 +399,10 @@ function main() {
     const TEXLIVE_BASE_URL = "https://texlive2.swiftlatex.com/pdftex/";
     const TEXLIVE_VERSION = '26';
     const TEMPLATES = { 
-        'DeThi': `\\documentclass[12pt]{article}\n\\usepackage[utf8]{vietnam}\n\\begin{document}\n\nĐây là mẫu đề thi.\n\n\\end{document}`, 
-        'VeHinh': `\\documentclass[12pt,tikz]{standalone}\n\\begin{document}\n\\begin{tikzpicture}\n\t% Vẽ hình ở đây\n\\end{tikzpicture}\n\\end{document}`, 
+        'DeThi': `\\documentclass[12pt]{article}\n\\usepackage[utf8]{vietnam}\n\n\\usepackage{polyglossia}\\begin{document}\n\nĐây là mẫu đề thi.\n\n\\end{document}`, 
+        'VeHinh': `\\documentclass[12pt,tikz]{standalone}\n\\usepackage{polyglossia}\n\\begin{document}\n\\begin{tikzpicture}\n\t% Vẽ hình ở đây\n\\end{tikzpicture}\n\\end{document}`, 
         'Beamer': `\\documentclass{beamer}\n\\usetheme{Madrid}\n\\title{Tiêu đề}\n\\author{Tác giả}\n\\begin{document}\n\\frame{\\titlepage}\n\\begin{frame}{Nội dung}\n\n\\end{frame}\n\\end{document}`, 
-        'book': `\\documentclass{book}\n\\usepackage[utf8]{vietnam}\n\\title{Tiêu đề sách}\n\\author{Tác giả}\n\\begin{document}\n\\frontmatter\n\\maketitle\n\\mainmatter\n\\chapter{Chương 1}\n\n\\end{document}` 
+        'book': `\\documentclass{book}\n\\usepackage[utf8]{vietnam}\n\\usepackage{polyglossia}\n\\title{Tiêu đề sách}\n\\author{Tác giả}\n\\begin{document}\n\\frontmatter\n\\maketitle\n\\mainmatter\n\\chapter{Chương 1}\n\n\\end{document}` 
     }; 
     const PREPACKAGED_FILES = { /* Có thể để trống hoặc thêm các file mặc định */ };
     const DEFAULT_SNIPPETS_JSON = `[ { "name": "Môi trường cơ bản", "type": "folder", "children": [ { "name": "Itemize", "type": "snippet", "content": "\\\\begin{itemize}\\n\\t\\\\item \\n\\\\end{itemize}" }, { "name": "Enumerate", "type": "snippet", "content": "\\\\begin{enumerate}\\n\\t\\\\item \\n\\\\end{enumerate}" } ] }, { "name": "Toán học", "type": "folder", "children": [ { "name": "Phân số", "type": "snippet", "content": "\\\\dfrac{$1}{$2}" } ] } ]`;
